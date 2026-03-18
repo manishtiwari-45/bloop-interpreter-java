155b4d769ee31884921a1568593a29c94f17ab01
Rathourdivyanshi6398 <divyanshirathour2006@gmail.com>
2026-03-18 18:50:51 +0530

    added comment

---
+++ src/lexer/Token.java
@@ -0,0 +1,35 @@
+package lexer;
+
+public class Token {
+
+    private final TokenType type;       // what kind of token
+    private final String value;         // actual text
+    private final int line;             // line number of source code
+
+    // Constructor
+    public Token(TokenType type, String value, int line){
+        this.type = type;
+        this.value = value;
+        this.line = line;
+    }
+
+    // Getters
+    public TokenType getType(){
+        return type;
+    }
+
+    public String getValue(){
+        return value;
+    }
+
+    public int getLine(){
+        return line;
+    }
+
+    // To print an object
+    @Override
+    public String toString(){
+        return "Token(" + type + ", \"" + value + "\", line=" + line + ")";
+    }
+    
+}
